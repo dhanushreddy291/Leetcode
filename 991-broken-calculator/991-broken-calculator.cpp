7179e81380e92ac921aa69e6952c3f8841797e9f
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2022-03-23 20:00:02 +0530

    Time: 763 ms (5.47%), Space: 5.8 MB (73.18%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int operations = 0;
+        while (target > startValue) {
+            if (target % 2 == 0) target /= 2;
+            else target++;
+            operations++;
+        }
+        while (target < startValue) {
+            target++;
+            operations++;        
+        }
+        return operations;
+    }
+};
