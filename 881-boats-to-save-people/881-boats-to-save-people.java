345d476e634e19d61ef382a8c1d20556322f909d
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2022-03-24 19:33:58 +0530

    Time: 31 ms (5.83%), Space: 65.2 MB (55.54%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.java
+++ 881-boats-to-save-people/881-boats-to-save-people.java
@@ -1,14 +1,9 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
-        int leftPointer = 0, rightPointer = people.length - 1, rescueBoats = 0;
-        while (leftPointer <= rightPointer) {
-            if (people[leftPointer] + people[rightPointer] > limit) rightPointer--;
-            else {
-                leftPointer++;
-                rightPointer--;
-            }
-            rescueBoats++;
+        int rescueBoats = 0;
+        for (int leftPointer = 0, rightPointer = people.length - 1; leftPointer <= rightPointer; rescueBoats++, rightPointer--) {
+            if (people[leftPointer] + people[rightPointer] <= limit) leftPointer++;
         }
         return rescueBoats;
     }
