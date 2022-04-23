a528915b2caa6769d6602029754672c9161d97cd
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2022-04-23 10:27:58 +0530

    Time: 9 ms (21.60%), Space: 7.5 MB (9.59%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        string code;
+        srand(time(0));
+        string code = "";
         while (url2code.find(longUrl) == url2code.end()) {
-            random_shuffle(alphabets.begin(), alphabets.end());
-            code = alphabets.substr(0, 6);
+            for (int i = 0; i < 6; i++) {
+                code += alphabets[rand() % 62];
+            }
             if (code2url.find(code) == code2url.end()) {
                 code2url[code] = longUrl;
                 url2code[longUrl] = code;
             }
         }
-        return "http://tinyurl.com/" + code;
+        return "http://bit.ly/" + code;
     }
 
     // Decodes a shortened URL to its original URL.
