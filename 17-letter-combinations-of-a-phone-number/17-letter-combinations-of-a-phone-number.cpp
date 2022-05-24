a118a59774259edaa5f869e56908b72cbb235a8e
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2022-05-24 21:34:09 +0530

    Time: 3 ms (39.74%), Space: 6.7 MB (18.51%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,27 +1,65 @@
 static string arr[] = {"","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 class Solution {
 private:
-    vector <string> helper(string s) {
-        if(s.length()==0){
-            vector<string> temp2;
-            temp2.push_back("");
-            return temp2;
-        }
-        char ch = s[0];
-        string x = arr[ch-'0'-1];
-        vector<string> temp1 = helper(s.substr(1));
-        vector<string> ans;
-        for(int j=0;j<x.length();j++){
-            char c = x[j];
-            for(int k=0;k<temp1.size();k++){
-                ans.push_back(c+temp1[k]);
-            }
-        }
-        return ans;
+void Permute (string &A, int i, vector <string> &Answer, string &str, map <int, vector <char>> &TelephoneButtons)
+{
+    int k = A[i] - '0';
+
+    if (i == A.size())
+    Answer.push_back (str);
+
+    else if (k < 2)
+    {
+        str += A[i];
+        Permute (A, i + 1, Answer, str, TelephoneButtons);
+        str.erase(str.end() - 1,str.end());
     }
+
+    else
+    {
+        for (int j = 0; j < TelephoneButtons[k].size(); j++)
+        {
+            str += TelephoneButtons[k][j];
+            Permute (A, i + 1, Answer, str, TelephoneButtons);
+            str.erase(str.end() - 1,str.end());
+        }
+    }
+
+    return;
+
+}
 public:
-    vector<string> letterCombinations(string s) {
-        if (s.length() == 0) return {};
-        else return helper(s);
-    }
+    vector<string> letterCombinations(string A) {
+
+    string str = "";
+    vector <string> Answer;
+    map <int, vector <char>> TelephoneButtons;
+
+    vector <char> temp1 = {'a', 'b', 'c'};
+    TelephoneButtons[2] = temp1;
+
+    vector <char> temp2 = {'d', 'e', 'f'};
+    TelephoneButtons[3]= temp2;
+
+    vector <char> temp3 = {'g', 'h', 'i'};
+    TelephoneButtons[4] = temp3;
+
+    vector <char> temp4 = {'j', 'k', 'l'};
+    TelephoneButtons[5] = temp4;
+    
+    vector <char> temp5 = {'m', 'n', 'o'};
+    TelephoneButtons[6] = temp5;
+
+    vector <char> temp6 = {'p', 'q', 'r', 's'};
+    TelephoneButtons[7] = temp6;
+
+    vector <char> temp7 = {'t', 'u', 'v'};
+    TelephoneButtons[8] = temp7;
+
+    vector <char> temp8 = {'w', 'x', 'y', 'z'};
+    TelephoneButtons[9] = temp8;
+    if (A.size() == 0) return Answer;
+    Permute (A, 0, Answer, str, TelephoneButtons);
+    return Answer;
+}
 };
