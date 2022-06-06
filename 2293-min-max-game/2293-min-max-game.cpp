15639b734122377f86800f67bd79481eeddfe40b
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-06 11:46:47 +0530

    Time: 26 ms (16.67%), Space: 9.8 MB (91.67%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.cpp
+++ 2293-min-max-game/2293-min-max-game.cpp
@@ -1,18 +1,16 @@
 class Solution {
 private:
-    int helper(vector <int> &nums) {
-        if (nums.size() == 1) return nums[0];
+    int helper(vector <int> &nums, int N) {
+        if (N == 1) return nums[0];
         else {
-            vector <int> temp;
-            for (int i = 0; i < nums.size() / 2; i++) {
-                if (i % 2) temp.push_back(max(nums[2 * i], nums[2 * i + 1]));
-                else temp.push_back(min(nums[2 * i], nums[2 * i + 1]));
+            for (int i = 0; i < N / 2; i++) {
+                nums[i] = (i % 2) ? max(nums[2 * i], nums[2 * i + 1]) : min(nums[2 * i], nums[2 * i + 1]);
             }
-            return helper(temp);
+            return helper(nums, N / 2);
         }
     }
 public:
     int minMaxGame(vector<int>& nums) {
-        return helper(nums);
+        return helper(nums, nums.size());
     }
 };
