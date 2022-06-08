c874334ab20205840128ec41facd9dd9ac944556
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-08 15:50:38 +0530

    Time: 0 ms (100.00%), Space: 6 MB (99.85%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -5,7 +5,7 @@ public:
         int count = 0;
         for (int i = 0; i < s.size() - 2; i++) {
             char a = s[i], b = s[i + 1], c = s[i + 2];
-            if (a == b || b == c || c == a) continue;
+            if (a == b || b == c || c == a) count = count;
             else count++;
         }
         return count;
