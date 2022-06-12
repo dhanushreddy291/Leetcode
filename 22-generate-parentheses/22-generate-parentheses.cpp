da0f254ddf65ead3f685cda7aa2f13c7273e698d
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-12 19:25:59 +0530

    Time: 11 ms (25.59%), Space: 13.8 MB (47.93%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,27 +1,17 @@
 class Solution {
 private:
-    void Generator(int L, int R) {
-        if (L == 0 && R == 0) Answer.push_back(Temp);
+    void Generator(int L, int R, string parenthesis) {
+        if (L == 0 && R == 0) Answer.push_back(parenthesis);
         else {
-            if (L > 0) {
-                Temp.push_back('(');
-                Generator(L - 1, R);
-                Temp.pop_back();
-            }
-            if (R - L > 0) {
-                Temp.push_back(')');
-                Generator(L, R - 1);
-                Temp.pop_back();
-            }
+            if (L > 0) Generator(L - 1, R, parenthesis + '(');
+            if (R - L > 0) Generator(L, R - 1, parenthesis + ')');
         }
     }
 public:
     vector <string> Answer;
-    string Temp;
     vector<string> generateParenthesis(int n) {
-        Temp = "";
         Answer.clear();
-        Generator(n, n);
+        Generator(n, n, "");
         return Answer;
     }
 };
