e24e98f92edba262fa611b69263fa6d11f019695
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-12 13:24:45 +0530

    Time: 3 ms (96.68%), Space: 9.5 MB (83.42%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.cpp
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -3,10 +3,15 @@ private:
     void Permute(string &s, vector <string> &Permutations, int i, int N) {
         if (i == N) Permutations.push_back(s);
         else if (isalpha(s[i])) {
-            s[i] = toupper(s[i]);
             Permute(s, Permutations, i + 1, N);
-            s[i] = tolower(s[i]);
-            Permute(s, Permutations, i + 1, N);
+            if (islower(s[i])) {
+                s[i] = toupper(s[i]);
+                Permute(s, Permutations, i + 1, N);
+            }
+            else {
+                s[i] = tolower(s[i]);
+                Permute(s, Permutations, i + 1, N);
+            }
         } else Permute(s, Permutations, i + 1, N);
     }
 public:
