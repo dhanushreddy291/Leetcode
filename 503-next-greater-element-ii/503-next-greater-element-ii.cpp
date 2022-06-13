f1237e6b812aa8e348a95e9c59212e391d9e4e7b
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-13 15:52:10 +0530

    Time: 47 ms (58.33%), Space: 24.7 MB (26.66%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -2,17 +2,16 @@ class Solution {
   public:
     vector < int > nextGreaterElements(vector < int > & nums) {
       int N = nums.size();
-      nums.insert(nums.end(), nums.begin(), nums.end());
       vector < int > nextGreaterElement(2 * N, -1);
       stack < int > Stack({
         nums[N - 1]
       });
-      for (int i = nums.size() - 2; i >= 0; i--) {
-        while (!Stack.empty() && Stack.top() <= nums[i]) {
+      for (int i = 2*N - 2; i >= 0; i--) {
+        while (!Stack.empty() && Stack.top() <= nums[i%N]) {
           Stack.pop();
         }
         nextGreaterElement[i] = (Stack.empty()) ? -1 : Stack.top();
-        Stack.push(nums[i]);
+        Stack.push(nums[i%N]);
       }
       nextGreaterElement.erase(N + nextGreaterElement.begin(), nextGreaterElement.end());
       return nextGreaterElement;
