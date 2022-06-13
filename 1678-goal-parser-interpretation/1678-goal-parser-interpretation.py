d8f0a76ddfbfdb30cee52955a78db7c3e48cc8c2
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-13 18:26:44 +0530

    Time: 49 ms (32.41%), Space: 13.8 MB (95.79%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,16 +1,15 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        Ans, i = "", 0
-        while i < len(command):
+        Ans, i, N = "", 0, len(command)
+        while i < N:
             if command[i] == '(':
                 if command[i + 1] == ')':
                     Ans += 'o'
-                    i += 2
                 else:
                     while command[i + 1] != ')':
                         Ans += command[i + 1]
                         i += 1
-                    i += 2
+                i += 2
             else:
                 Ans += command[i]
                 i += 1
