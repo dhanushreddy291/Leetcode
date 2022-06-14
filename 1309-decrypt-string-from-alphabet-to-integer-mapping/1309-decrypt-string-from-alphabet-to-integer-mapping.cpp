980f07b94524afabbd0964dc32ce631b510b00c8
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-14 22:13:32 +0530

    Time: 3 ms (45.90%), Space: 6.2 MB (76.23%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -4,10 +4,7 @@ public:
         int N = s.size(), i = -1;
         string Ans = "";
         while (++i < N) {
-            if (i + 2 < N && s[i + 2] == '#') {
-                Ans += ('j' + 10 * (s[i] - '0') + (s[i + 1] - '0') - 10);
-                i += 2;
-            }
+            if (i + 2 < N && s[i + 2] == '#') Ans += ('j' + 10 * (s[i++] - '0') + (s[i++] - '0') - 10);
             else Ans += ('a' + s[i] - '1');
         }
         return Ans;
