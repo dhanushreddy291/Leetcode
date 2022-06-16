8a9f33faa717666a7594a04fbf8df2c82cce8ef6
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-16 12:19:25 +0530

    Time: 48 ms (68.05%), Space: 6.9 MB (97.74%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,19 +1,21 @@
 class Solution {
+private:
+    int maxPalindrome(int i, int j, int N, string &s) {
+        if (i < 0 || j >= N) return 0;
+        else if (s[i] == s[j]) return 2 + maxPalindrome(i - 1, j + 1, N, s);
+        else return 0;
+    }
 public:
     string longestPalindrome(string s) {
-        int N = s.size(), start = 0, maxLength = 0;
-        vector <vector <bool>> DP(N, vector <bool> (N));
-        for (int length = 0; length < N; length++) {
-            for (int i = 0, j = length; j < N; i++, j++) {
-                if (i == j) DP[i][j] = true;
-                else if (j - i == 1) DP[i][j] = (s[i] == s[j]);
-                else DP[i][j] = (s[i] == s[j] && DP[i + 1][j - 1]);
-                if (DP[i][j] and j - i + 1 > maxLength) {
-                    maxLength = j - i + 1;
-                    start = i;
-                }
+        int maxLength = 1, N = s.size(), index = 0;
+        for (int i = 0; i < N - 1; i++) {
+            int palindromeLength = 1 + maxPalindrome(i - 1, i + 1, N, s);
+            if (s[i] == s[i + 1]) palindromeLength = max(palindromeLength, 2 + maxPalindrome(i - 1, i + 2, N, s));
+            if (palindromeLength > maxLength) {
+                maxLength = palindromeLength;
+                index = i;
             }
         }
-        return s.substr(start, maxLength);
+        return maxLength & 1 ? s.substr(index - maxLength/2, maxLength) : s.substr(index - maxLength/2 + 1, maxLength);
     }
 };
