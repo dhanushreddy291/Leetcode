f2859729c8dbf2f4c3f9108e054a1ec5de428383
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-19 08:42:55 +0530

    Time: 99 ms (47.64%), Space: 14.6 MB (35.23%) - LeetHub

---
+++ 841-keys-and-rooms/841-keys-and-rooms.py
@@ -0,0 +1,17 @@
+import queue
+
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        N, Queue = len(rooms), queue.Queue() 
+        Visited = [False for x in range(N)]
+        for room in rooms[0]:
+            Queue.put(room)
+            Visited[room] = True
+        Visited[0] = True
+        while Queue.qsize() > 0:
+            roomNum = Queue.get()
+            for room in rooms[roomNum]:
+                if not Visited[room]:
+                    Queue.put(room)
+                    Visited[room] = True
+        return False if False in Visited else True
