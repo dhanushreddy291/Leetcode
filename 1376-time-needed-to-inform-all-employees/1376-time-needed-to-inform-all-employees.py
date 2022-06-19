fd836052ad846a466fbecdc512ce26c04e4eaef1
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-19 22:17:43 +0530

    Time: 2142 ms (37.57%), Space: 36.8 MB (88.44%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,18 +1,14 @@
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
 
-        def DFS(node, currTime, maxTime, graph, informTime):
-            maxTime[0] = max(maxTime[0], currTime)
-            for subNode in graph[node]:
-                DFS(subNode, currTime + informTime[node], maxTime, graph, informTime)
-
-        maxTime = [0]
         N = len(manager)
         graph = [[] for i in range(n)]
+        
         for i in range(N):
             if manager[i] != -1:
                 graph[manager[i]].append(i)
 
-        DFS(headID, 0, maxTime, graph, informTime)
-        
-        return maxTime[0]
+        def DFS(node, currTime, graph, informTime):
+            return max([DFS(subNode, currTime + informTime[node], graph, informTime) for subNode in graph[node]]) if len(graph[node]) > 0 else currTime
+
+        return DFS(headID, 0, graph, informTime)
