3a0692c9ae98b3022f2457e637115eb2a112290d
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-22 15:43:02 +0530

    Time: 190 ms (5.16%), Space: 15.5 MB (10.14%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,11 +1,22 @@
-import bisect
-
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        index1 = bisect.bisect_left(nums, target)
-        index2 = bisect.bisect_right(nums, target)
-        N = len(nums)
-        if not 0 <= index1 < N or nums[index1] != target:
-            return [-1, -1]
-        else:
-            return [index1, index2 - 1]
+
+        def findStarting(nums, n):
+            N = len(nums)
+            lo, hi = 0, N - 1
+            while lo <= hi:
+                mid = (lo + hi) // 2
+                if nums[mid] >= n: hi = mid - 1
+                else: lo = mid + 1
+            return lo if lo < len(nums) and nums[lo] == target else -1
+
+        def findEnding(nums, n):
+            N = len(nums)
+            lo, hi = 0, N - 1
+            while lo <= hi:
+                mid = (lo + hi) // 2
+                if nums[mid] <= n: lo = mid + 1
+                else: hi = mid - 1
+            return hi if nums[hi] == target else -1
+
+        return [findStarting(nums, target), findEnding(nums, target)] if len(nums) > 0 else [-1, -1]
