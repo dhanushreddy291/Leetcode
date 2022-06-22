6009375e15fbe0982392b0a667edefb07ea38946
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-22 12:55:24 +0530

    Time: 122 ms (66.05%), Space: 13.9 MB (79.71%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.py
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.py
@@ -6,14 +6,8 @@ class Solution:
         distanceValue = 0
 
         for num in arr1:
-            index1, index2 = bisect_left(arr2, num + d), bisect_left(arr2, num - d)
-            if 0 <= index1 < N:
-                if arr2[index1] == num + d:
-                    continue
-            if 0 <= index2 < N:
-                if arr2[index2] == num - d:
-                    continue            
-            if  index1 == index2:
+            index1, index2 = bisect_left(arr2, num + d), bisect_left(arr2, num - d)  
+            if  index1 == index2 and arr2[index1 % N] != num + d:
                 distanceValue += 1
 
         return distanceValue
