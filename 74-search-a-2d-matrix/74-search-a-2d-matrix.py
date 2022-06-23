91530e2e0442b2136ffcdbe7c356ee2a04499f62
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-23 10:00:19 +0530

    Time: 55 ms (70.91%), Space: 14.4 MB (88.83%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows, columns = len(matrix), len(matrix[0])
+        start, end = 1, rows * columns
+
+        def coordinatesFindKaro(N, M, Val):
+            return (max((Val // M) - 1, 0) if Val % M == 0 else Val // M, M - 1 if Val % M == 0 else max((Val % M) - 1, 0))
+
+        while start <= end:
+            mid = (start + end) // 2
+            (x, y) = coordinatesFindKaro(rows, columns, mid)
+            if matrix[x][y] == target: return True
+            elif matrix[x][y] > target: end = mid - 1
+            else: start = mid + 1
+        return False
