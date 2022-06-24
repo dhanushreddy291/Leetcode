a73cc71dce9b9bb24dd6bdb5d8d875cbf421297b
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-24 22:19:50 +0530

    Time: 18 ms (10.74%), Space: 10.4 MB (46.98%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,9 +1,10 @@
+#include "valarray"
+
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        vector <int> Answer;
         unordered_map <int, int> Map;
-        int N = nums1.size(), M = nums2.size();
+        int N = nums1.size(), M = nums2.size(), j = 0;
         for (int i = 0; i < N; i++) {
             Map[nums1[i]]++;
         }
@@ -11,10 +12,10 @@ public:
             if (Map.find(nums2[i]) != Map.end()) {
                 if (Map[nums2[i]] > 0) {
                     Map[nums2[i]]--;
-                    Answer.push_back(nums2[i]);   
+                    nums1[j++] = nums2[i];   
                 }
             }
         }
-        return Answer;
+        return vector <int> (nums1.begin(), nums1.begin() + j);
     }
 };
