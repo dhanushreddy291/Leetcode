266116d48dca68c03cef9dc3c5608820447902d7
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-06-27 12:36:29 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (89.79%) - LeetHub

---
+++ 415-add-strings/415-add-strings.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+private:
+    string add(string &a, string &b, int base)
+    {
+        int n = a.size();
+        string ans = "";
+        int carry = 0, j = b.size() - 1;
+        for (int i = n - 1; j >= 0; i--, j--)
+        {
+            int A = 0, B = (b[j] - '0');
+            if (i >= 0)
+            {
+                A = (a[i] - '0');
+            }
+            int sum = A + B + carry;
+            ans += (sum % base + '0');
+            carry = sum / base;
+        }
+        if (carry > 0)
+            ans += (carry + '0');
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+
+    string stringAdder(string &a, string &b, int base)
+    {
+        if (a.size() < b.size())
+            return add(a, b, base);
+        return add(b, a, base);
+    }
+
+public:
+    string addStrings(string num1, string num2)
+    {
+        return stringAdder(num1, num2, 10);
+    }
+};
