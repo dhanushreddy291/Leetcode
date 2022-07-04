03a322e67eab504853e2edf3795915389223c53c
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-07-04 16:05:41 +0530

    Time: 4 ms (70.03%), Space: 51.7 MB (55.41%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -1,20 +1,25 @@
 class Solution {
     public int candy(int[] ratings) {
-        int N = ratings.length;
-        int[] candies = new int[N];
-        candies[0] = 1;
-        for (int i = 1; i < N; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                candies[i] = 1 + candies[i - 1];
-            } else candies[i] = 1;
+        
+        int Sum = 0;
+        int[] candies = new int[ratings.length];
+
+        for (int i = 0; i < ratings.length; i++) {
+            candies[i] = 1;
         }
-        int Sum = candies[N - 1];
-        for (int i = N - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
-                candies[i] = 1 + candies[i + 1];
-            }
+
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) candies[i] = 1 + candies[i - 1];
+        }
+
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) candies[i] = Math.max(1 + candies[i + 1], candies[i]);
+        }
+
+        for (int i = 0; i < ratings.length; i++) {
             Sum += candies[i];
         }
+
         return Sum;
     }
 }
