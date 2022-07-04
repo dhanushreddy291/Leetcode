17815aa8a4f9035e37d581b942a9d443275efe67
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-07-04 13:22:03 +0530

    Time: 254 ms (46.13%), Space: 16.8 MB (74.92%) - LeetHub

--- 135-candy/135-candy.py
+++ 135-candy/135-candy.py
@@ -5,7 +5,6 @@ class Solution:
         for i in range(1, N):
             if ratings[i] > ratings[i - 1]:
                 candies[i] = 1 + candies[i - 1]
-        print(candies)
         for i in range(N - 2, -1, -1):
             if ratings[i] > ratings[i + 1] and candies[i] <= candies[i + 1]:
                 candies[i] = 1 + candies[i + 1]
