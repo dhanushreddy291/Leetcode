4107e23512f4a3f6a63c4522e336280e29a811de
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-07-07 22:41:55 +0530

    Time: 291 ms (19.98%), Space: 16.8 MB (20.12%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,3 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return reduce(lambda x, y: x ^ y, nums)
+        xor = 0
+        for num in nums:
+            xor = xor ^ num
+        return xor
+        
