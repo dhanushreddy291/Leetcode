dcbe6263c93febcacf73d85d74196bddf8dc09b7
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-07-11 20:03:40 +0530

    Time: 82 ms (5.25%), Space: 19.6 MB (46.12%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -5,7 +5,7 @@ private:
     }
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector <vector <bool>> Row(9, vector <bool> (9, false)), Column(9, vector <bool> (9, false)), Grid(9, vector <bool> (9, false));
+        vector <vector <bool>> Row(9, vector <bool> (9, false)), Column(Row), Grid(Row);
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] != '.') {
