faa95564475144005dbcb7ff8fe1d7259b54464f
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-07-15 23:06:49 +0530

    Time: 78 ms (77.58%), Space: 13.8 MB (80.27%) - LeetHub

--- 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.py
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.py
@@ -3,4 +3,6 @@ class Solution:
         rep, norm = 6, 6
         for i in range(n - 1):
             rep, norm = 3 * rep + 2 * norm, 2 * rep + 2 * norm
+            rep = rep % 1000000007
+            norm = norm % 1000000007
         return (rep + norm) % 1000000007
