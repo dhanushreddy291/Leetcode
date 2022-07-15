5b89b2563e900a7e13b33960b6edb7fe4c586422
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-07-15 19:16:38 +0530

    Time: 61 ms (43.19%), Space: 14.2 MB (49.58%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.py
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -1,21 +1,10 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        N = len(nums)
-        count, size = 0, N - 3
-        if N > 2:
-            isArithmetic = [0] * (N - 2)
-            for j in range(2, N):
-                isArithmetic[j - 2] = 1 if nums[j - 2] + nums[j] == 2 * nums[j - 1] else 0
-                count += isArithmetic[j - 2]
-            while size > 0:
-                localCount = 0
-                for i in range(size):
-                    isArithmetic[i] *= isArithmetic[i + 1]
-                    localCount += isArithmetic[i]
-                if localCount == 0:
-                    return count
-                else:
-                    count += localCount
-                isArithmetic.pop()
-                size -= 1
-        return count
+        currCount, totalCount = 0, 0
+        for i in range(2, len(nums)):
+            if nums[i] + nums[i - 2] == 2 * nums[i - 1]:
+                currCount += 1
+                totalCount += currCount
+            else:
+                currCount = 0
+        return totalCount
