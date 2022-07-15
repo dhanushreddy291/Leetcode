2295afd2467644781bc754343b52020a31a700ff
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-07-15 19:00:56 +0530

    Time: 83 ms (9.15%), Space: 14.2 MB (11.99%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.py
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -1,23 +1,21 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
         N = len(nums)
-        if N < 3:
-            return 0
-        isArithmetic = [0] * (N - 2)
-        i, count, size = 0, 0, N - 3
-        i = 0
-        for j in range(2, N):
-            isArithmetic[i] = 1 if nums[i] + nums[j] == 2 * nums[i + 1] else 0
-            count += isArithmetic[i]
-            i += 1
-        while size > 0:
-            localCount = 0
-            for i in range(size):
-                isArithmetic[i] *= isArithmetic[i + 1]
-                localCount += isArithmetic[i]
-            if localCount == 0:
-                return count
-            else:
-                count += localCount
-            size -= 1
+        count, size = 0, N - 3
+        if N > 2:
+            isArithmetic = [0] * (N - 2)
+            for j in range(2, N):
+                isArithmetic[j - 2] = 1 if nums[j - 2] + nums[j] == 2 * nums[j - 1] else 0
+                count += isArithmetic[j - 2]
+            while size > 0:
+                localCount = 0
+                for i in range(size):
+                    isArithmetic[i] *= isArithmetic[i + 1]
+                    localCount += isArithmetic[i]
+                if localCount == 0:
+                    return count
+                else:
+                    count += localCount
+                isArithmetic.pop()
+                size -= 1
         return count
