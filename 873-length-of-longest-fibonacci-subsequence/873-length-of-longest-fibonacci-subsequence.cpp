041f25eb85f25bf333a15c503ed4bf20cc966f63
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-07-22 11:28:56 +0530

    Time: 2039 ms (5.05%), Space: 14.5 MB (54.38%) - LeetHub

--- 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.cpp
+++ 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.cpp
@@ -1,20 +1,25 @@
 class Solution {
 private:
-    int helper(vector<int> &A, vector <vector<int>> &DP, int i, int j, int N) {
+    int DP[1000][1000];
+    int helper(vector<int> &A, int i, int j, int N) {
         if (j >= N) return 0;
-        if (DP[i][j] != -1) return DP[i][j];
+        if (DP[i][j] != 0) return DP[i][j];
         int Val = A[i] + A[j];
         int idx = lower_bound(A.begin() + j, A.end(), Val) - A.begin();
-        if (idx == N || A[idx] != Val) return DP[i][j] = 2;
-        return DP[i][j] = 1 + helper(A, DP, j, idx, N);
+        if (idx == N || A[idx] != Val) {
+            DP[i][j] = 2;
+            return 2;   
+        }
+        DP[i][j] = 1 + helper(A, j, idx, N);
+        return DP[i][j];
     }
 public:
     int lenLongestFibSubseq(vector<int>& A) {
         int N = A.size(), Ans = 0;
-        vector <vector<int>> DP(N, vector <int> (N, -1));
+        memset(DP, 0, sizeof(DP));
         for (int i = 0; i < N; i++) {
             for (int j = i + 1; j < N; j++) {
-                Ans = max(Ans, helper(A, DP, i, j, N));
+                Ans = max(Ans, helper(A, i, j, N));
             }
         }
         return (Ans > 2) ? Ans : 0;
