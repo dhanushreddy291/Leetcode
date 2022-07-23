cceb9f787e3e8bea47dcbb5df5c79261156512a2
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-07-23 17:12:05 +0530

    Time: 178 ms (28.50%), Space: 14.6 MB (36.97%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,21 +2,14 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         sort(coins.begin(), coins.end());
-        vector <int> minCoinsRequired(amount + 1);
+        vector <int> minCoinsRequired(amount + 1, amount + 1);
         minCoinsRequired[0] = 0;
         for (int i = 1; i <= amount; i++) {
-            minCoinsRequired[i] = INT_MAX;
             for (int j = 0; j < coins.size(); j++) {
-                if (i - coins[j] < 0) break;
-                if (i == coins[j]) {
-                    minCoinsRequired[i] = 1;
-                    break;
-                }
-                if (minCoinsRequired[i - coins[j]] != INT_MAX) {
-                    minCoinsRequired[i] = min(minCoinsRequired[i], 1 + minCoinsRequired[i - coins[j]]);
-                }
+                if (i < coins[j]) break;
+                minCoinsRequired[i] = min(minCoinsRequired[i], 1 + minCoinsRequired[i - coins[j]]);
             }
         }
-        return minCoinsRequired[amount] == INT_MAX ? -1: minCoinsRequired[amount];
+        return minCoinsRequired[amount] > amount ? -1: minCoinsRequired[amount];
     }
 };
