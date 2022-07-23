e20d829010cefd0ea747318fdabb68cc981161f4
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-07-23 17:01:23 +0530

    Time: 194 ms (23.41%), Space: 14.6 MB (36.97%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        unordered_set <int> Set;
+        for (int i = 0; i < coins.size(); i++) {
+            Set.insert(coins[i]);
+        }
+        sort(coins.begin(), coins.end());
+        vector <int> minCoinsRequired(amount + 1);
+        minCoinsRequired[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            if (Set.find(i) != Set.end()) {
+                minCoinsRequired[i] = 1;
+            }
+            else {
+                minCoinsRequired[i] = INT_MAX;
+                for (int j = 0; j < coins.size(); j++) {
+                    if (i - coins[j] < 0) break;
+                    if (minCoinsRequired[i - coins[j]] == INT_MAX) continue;
+                    minCoinsRequired[i] = min(minCoinsRequired[i], 1 + minCoinsRequired[i - coins[j]]);
+                }
+            }
+        }
+        return minCoinsRequired[amount] == INT_MAX ? -1: minCoinsRequired[amount];
+    }
+};
